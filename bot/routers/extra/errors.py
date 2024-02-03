82ae6b40f6ae02080cb3b8c4504b2702d0971f37
Dmitrii <lovelybelyashik@gmail.com>
2024-02-03 21:07:26 +0300

    Add close keyboard to error message

--- bot/routers/extra/errors.py
+++ bot/routers/extra/errors.py
@@ -8,6 +8,7 @@ from aiogram_i18n import I18nContext
 
 from ...database.models import User
 from ...utils.notify import sender
+from ...keyboards import inline
 
 router: Final[Router] = Router(name=__name__)
 
@@ -17,4 +18,4 @@ async def handle_some_error(error: ErrorEvent, i18n: I18nContext, user: User):
     await sender.error_notify(user_id=user.user_id,
                                 mention=error.update.message.from_user.mention_html(),
                                 exception=error.exception)
-    return error.update.message.answer(text=i18n.errors.error())
+    return error.update.message.answer(text=i18n.errors.error(), reply_markup=inline.close())
