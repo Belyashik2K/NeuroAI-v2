68bc81788d43ce4b1537da9cb876547eedf0157f
Dmitrii <lovelybelyashik@gmail.com>
2024-04-18 12:05:40 +0300

    Migrate Midjourney v6 to VisionCraft API

--- bot/routers/main/neuros/request.py
+++ bot/routers/main/neuros/request.py
@@ -307,37 +307,6 @@ async def tencentmaker(message: types.Message, user: User, state: FSMContext, i1
     await database.update_user(user_id=user.user_id, request_counter=user.request_counter + 1)
     await state.clear()
 
-
-@router.message(NeuroRequest.midjourneyv6, F.text)
-async def midjourneyv6(message: types.Message, user: User, state: FSMContext, i18n: I18nContext):
-    await message.delete()
-    data = await state.get_data()
-    formatting = {
-        "neuro": LazyProxy(f"buttons-{data['neuro']}").data,
-        'prompt': message.text
-    }
-    await message.bot.edit_message_text(i18n.messages.image_processing(**formatting),
-                                        chat_id=message.chat.id,
-                                        message_id=data['message_id'])
-    result = await future.midjourneyv6(message.text)
-    await message.bot.delete_message(message.chat.id, data['message_id'])
-
-    photos = list()
-
-    for url in result:
-        photos.append(types.InputMediaPhoto(media=types.URLInputFile(url, filename='photo.png')))
-
-    async with ChatActionSender.upload_photo(bot=message.bot, chat_id=message.chat.id):
-        m = await message.bot.send_media_group(chat_id=message.chat.id,
-                                               media=photos)
-    await message.answer(reply_to_message_id=m[-1].message_id,
-                         text=i18n.messages.image_result(**formatting),
-                         reply_markup=await inline.close_or_again(data['neuro']),
-                         parse_mode=ParseMode.MARKDOWN)
-    await database.update_user(user_id=user.user_id,
-                               request_counter=user.request_counter + 1)
-    await state.clear()
-
 @router.message(NeuroRequest.t2g, F.text)
 async def t2g(message: types.Message, user: User, state: FSMContext, i18n: I18nContext):
     await message.delete()
